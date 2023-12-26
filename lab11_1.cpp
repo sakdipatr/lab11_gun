14a33bca79b8ad9b154d5033a425a387d63eae9a
gun <sakfipat@gmail.com>
2023-12-26 14:25:10 +0700

    ad

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.\n";
+    string alpha[9] = {"A","B+","B","C+","C","D","F","W"};
+                          
+    cin.get();
+    cin.get();
+    cin.get();
+
+    int x = rand()%9;
+    cout << "You will get " << alpha[x] <<" in this 261102.";
+    
+    return 0;
+}
